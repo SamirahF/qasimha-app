7be104ff5a905f46440271812c436c535f05b5aa
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-02 09:06:56 +0300

    add user model

--- backend/qasimha/qasimha_app/models.py
+++ backend/qasimha/qasimha_app/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of @ followed by at least three alphanumericals'
+        )]
+    )
+    first_name = models.CharField (max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField (unique=True, blank=False)
