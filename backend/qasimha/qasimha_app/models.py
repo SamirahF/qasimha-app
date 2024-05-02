c266f42ab0ea85219b89d1dcccb53545e83020de
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-02 10:29:00 +0300

    migrate database

--- backend/qasimha/qasimha_app/models.py
+++ backend/qasimha/qasimha_app/models.py
@@ -4,13 +4,13 @@ from django.core.validators import RegexValidator
 
 class User(AbstractUser):
     username = models.CharField(
-        max_length=50,
+        max_length=40,
         unique=True,
         validators=[RegexValidator(
             regex=r'^@\w{3,}$',
             message='Username must consist of @ followed by at least three alphanumericals'
         )]
     )
-    first_name = models.CharField (max_length=100, blank=False)
+    first_name = models.CharField(max_length=100, blank=False)
     last_name = models.CharField(max_length=100, blank=False)
-    email = models.EmailField (unique=True, blank=False)
+    email = models.EmailField(unique=True, blank=False)
