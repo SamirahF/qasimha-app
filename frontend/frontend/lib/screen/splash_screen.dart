d15464ff74ce7df8ac4741c3c5fbf30ec3d0d5fe
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-03 17:40:38 +0300

    add intial template of start screens

--- frontend/frontend/lib/screen/splash_screen.dart
+++ frontend/frontend/lib/screen/splash_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
-import 'package:frontend/main.dart';
+import 'package:frontend/screen/start_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,12 +17,8 @@ class SplashScreenState extends State<SplashScreen>
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
-      print('print');
-      Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (_) => const MyHomePage(
-          title: 'home page',
-        ),
-      ));
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => StartScreen()));
     });
   }
 
