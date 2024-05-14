a7664debd1bcd5a23d6cfd61639b221c56e81ef3
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-14 21:18:35 +0300

    add constant file

---
+++ frontend/frontend/lib/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// primary colors
+const primary1 = Color.fromARGB(255, 177, 188, 230);
+const primary2 = Color.fromARGB(255, 70, 173, 176);
+const primary3 = Color.fromARGB(255, 212, 180, 144);
