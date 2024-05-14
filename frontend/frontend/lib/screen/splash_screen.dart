76a5e9bbf38a837a68394d3e900af9d2bf17faa9
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-14 20:24:49 +0300

    add start-screen

--- frontend/frontend/lib/screen/splash_screen.dart
+++ frontend/frontend/lib/screen/splash_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-import 'package:frontend/screen/sign_up.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:frontend/screen/start_screen.dart';
+import 'package:frontend/screen/start_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -20,7 +18,7 @@ class SplashScreenState extends State<SplashScreen>
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => SignUpScreen()));
+          .pushReplacement(MaterialPageRoute(builder: (_) => StartScreen()));
     });
   }
 
