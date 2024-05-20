050db9abc92a944059adbf4950aa090dcbb05cce
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-05-20 12:22:48 +0300

    add login functionality

--- backend/qasimha/core/views/auth_views/user_views.py
+++ backend/qasimha/core/views/auth_views/user_views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.hashers import check_password
 
 User = get_user_model()
 
@@ -13,17 +14,29 @@ class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        user = User(
+            username=data.get('username'),
+            email=data.get('email'),
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name')
+        )
+        user.set_password(data.get('password'))  # Hash the password
+        user.save()
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     @action(detail=False, methods=['post'], url_path='login')
     def login_user(self,request):
         email = request.data.get('email')
         password = request.data.get('password')
-        try:
+        try: 
                 user = User.objects.get(email=email)
-                print(user.password)
-                print(user.username)
+                print(check_password(password, user.password))
                 print("authentication with username:" , authenticate(username =user.username, password=password))
                 if user is not None:
-                    print(user)
+                    # print(user)
                     serializer = UserSerializer(user)
                     return Response(serializer.data)
                 else:
